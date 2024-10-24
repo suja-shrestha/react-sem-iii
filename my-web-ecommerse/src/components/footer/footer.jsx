4efb91f0e17be24b2b613ea8e4ef95180bbc4d4b
suja-shrestha <shresthasujal917@gmail.com>
2024-10-24 20:21:13 +0545

    24-10-2024

--- my-web-ecommerse/src/components/footer/footer.jsx
+++ my-web-ecommerse/src/components/footer/footer.jsx
@@ -1,48 +1,55 @@
+import './footer.css'
 function Khuta() {
     return (
         <>
-            <div class="fooer">
+            <div class="footer">
                 <div class="grid-item">
                     <p>Features</p>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>Secure Payment Options</li>
+                        <li>Fast Shipping Worldwide</li>
+                        <li>24/7 Customer Support</li>
+                        <li>Easy Returns & Exchanges</li>
+                        <li>Exclusive Member Discounts</li>
+                        
                     </ul>
                 </div>
                 <div class="grid-item">
                     <p>Featured Products</p>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>Stylish Apparel</li>
+                        <li>Latest Electronics</li>
+                        <li>Home Essentials</li>
+                        <li>Health & Beauty</li>
+                        <li>Sports Equipment</li>
                     </ul>
                 </div>
                 <div class="grid-item">
                     <p>About Us</p>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>Our Mission</li>
+                        <li>Our Values</li>
+                        <li>Sustainability Efforts</li>
+                        <li>Meet the Team</li>
+                        <li>Careers</li>
                     </ul>
                 </div>
                 <div class="grid-item">
                     <p>Follow Us</p>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>Facebook</li>
+                        <li>Instagram</li>
+                        <li>Twitter</li>
+                        <li>Pinterest</li>
+                        <li>LinkedIn</li>
                     </ul>
                 </div>
             </div>
+            <div className="end">
+    <i className="bi bi-globe-europe-africa"></i>
+    <p>Your E-Commerce Partner</p>
+</div>
+
 
         </>
     )
