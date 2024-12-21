600146c10718cefdc8e8735d947fb2438e8b81f1
suja-shrestha <shresthasujal917@gmail.com>
2024-12-21 21:19:00 +0545

    complete

--- youtube-clone/src/Components/Navbar/navbar.jsx
+++ youtube-clone/src/Components/Navbar/navbar.jsx
@@ -3,9 +3,7 @@ import "./navbar.css";
 import menu_icon from "../../assets/menu.png";
 import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search.png";
-import upload_icon from "../../assets/upload.png";
-import more_icon from "../../assets/more.png";
-import notification_icon from "../../assets/notification.png";
+import upload_icon from "../../assets/upload.png";git ;
 import profile_icon from "../../assets/jack.png";
 
 const navbar = ({setSidebar}) => {
